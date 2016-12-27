833128bb0c4cde50b671763b1128d6262a494cc7
xxccmd <a564268706@outlook.com>
2016-12-27 15:56:03 +0800

    m9 done

---
+++ app/src/main/java/edu/gdmec/s07150815/myguard/m1home/entity/VersionEntity.java
@@ -0,0 +1,16 @@
+package edu.gdmec.s07150815.myguard.m1home.entity;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class VersionEntity  {
+    //服务器版本号
+    public  String versioncode;
+    //版本描述
+    public String description;
+    //apk下载地址
+    public String apkurl;
+
+}
